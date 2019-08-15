e9a81598061d163c66fe7c3687000a5128d4c464
yeroc8 <yeroc8.git@gmail.com>
2019-08-15 15:22:57 -0400

    Copy some stuff from simple_connect.ino

--- main.ino
+++ main.ino
@@ -4,9 +4,12 @@
 RPLidar lidar;
 LewanSoul servo;
 
+#define RPLIDAR_MOTOR 3 // PWM pin connected to MOTOCTRL signal
+
 // runs once when rover is powered up
 void setup() {
-    ;
+    lidar.begin(Serial);
+    pinMode(RPLIDAR_MOTOR, OUTPUT)
 }
 
 // runs constantly as long as rover is powered
